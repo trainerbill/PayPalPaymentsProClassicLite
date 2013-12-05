7e6784cd091506d5e6282b84200058bda9141139
Andrew Throener <athroener@andrew-ubuntu-virtualBox.(none)>
2013-12-05 12:07:31 -0600

    added version to getexpresscheckout example

--- examples/getexpresscheckout.php
+++ examples/getexpresscheckout.php
@@ -20,6 +20,7 @@ $getec->setCredentials($credentials);
 //Place any variables into this array:  https://developer.paypal.com/webapps/developer/docs/classic/api/merchant/SetExpressCheckout_API_Operation_NVP/
 $variables = array(
 		'TOKEN' => '',
+		'VERSION' => '109.0'
 );
 
 //Place the variables onto the stack
